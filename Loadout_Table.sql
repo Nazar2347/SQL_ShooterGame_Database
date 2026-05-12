86eaee8e1105c6cb8bab57eaa5453c2884bd3ac3
KanRuto <dawkapspo@gmail.com>
2026-05-12 15:51:47 +0200

    Create Loadout_Table.sql

---
+++ Loadout_Table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE LOADOUT(
+	ladout_id INT IDENTITY(1,1) PRIMARY KEY,
+	player_id INT FOREIGN KEY REFERENCES Player(player_id),
+	item_id INT FOREIGN KEY REFERENCES Items(item_id),
+	loadout_name VARCHAR(20) NOT NULL,
+	primary_weapeon_id INT FOREIGN KEY REFERENCES Items(item_id),
+	secondary_weapeon_id INT FOREIGN KEY REFERENCES Items(item_id)
+);
