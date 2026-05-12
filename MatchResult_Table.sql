55db2fba0d78d51f9258956f53893f33182eb177
Nazarii Dunas <dunas.nazar@gmail.com>
2026-05-12 16:12:22 +0200

    Inserted data to tables

--- MatchResult_Table.sql
+++ MatchResult_Table.sql
@@ -9,3 +9,37 @@ CREATE TABLE Match_Result
 	FOREIGN KEY (match_id) REFERENCES Matches(match_id),
 	FOREIGN KEY (player_id) REFERENCES Player(player_id)
 )
+
+-- AI generated data insertion for Match_Result table
+DECLARE @PlayerID INT = 1;
+DECLARE @MatchesToPlay INT;
+DECLARE @CurrentMatch INT;
+
+-- Loop through all 50 players
+WHILE @PlayerID <= 49
+BEGIN
+    -- Decide how many matches this player will play (Random number between 1 and 5)
+    -- ABS(CHECKSUM(NEWID())) is the best way to get a truly random number in MSSQL
+    SET @MatchesToPlay = ABS(CHECKSUM(NEWID())) % 5 + 1; 
+
+    SET @CurrentMatch = 1;
+
+    -- Inner loop: insert that specific number of matches for the current player
+    WHILE @CurrentMatch <= @MatchesToPlay
+    BEGIN
+        INSERT INTO Match_Result (match_id, player_id, kills, deaths, is_winner)
+        VALUES (
+            ABS(CHECKSUM(NEWID())) % 10 + 1, -- Random match_id between 1 and 10
+            @PlayerID,                       -- The current player in the loop
+            ABS(CHECKSUM(NEWID())) % 35,     -- Random kills between 0 and 34
+            ABS(CHECKSUM(NEWID())) % 20,     -- Random deaths between 0 and 19
+            ABS(CHECKSUM(NEWID())) % 2       -- Random 0 (Loss) or 1 (Win)
+        );
+
+        -- Move to the next match for this player
+        SET @CurrentMatch = @CurrentMatch + 1;
+    END
+
+    -- Move to the next player
+    SET @PlayerID = @PlayerID + 1;
+END
--- Matches_Table.sql
+++ Matches_Table.sql
@@ -1,7 +1,19 @@
-CREATE TABLE Matches 
+/*CREATE TABLE Matches 
 (
 	match_id INT PRIMARY KEY IDENTITY(1,1),
 	map_name VARCHAR (25) NOT NULL,
 	start_time DATETIME DEFAULT GETDATE(),
 	end_time DATETIME,
-)
+)*/
+INSERT INTO Matches (map_name, start_time, end_time)
+VALUES 
+    ('Dust 2', '2026-05-10 14:00:00', '2026-05-10 14:25:30'),      -- 25 min match
+    ('Nuketown', '2026-05-10 15:15:00', '2026-05-10 15:27:10'),    -- 12 min match
+    ('Rust', '2026-05-11 09:30:00', '2026-05-11 09:40:45'),        -- 10 min match
+    ('Terminal', '2026-05-11 11:00:00', '2026-05-11 11:35:00'),    -- 35 min match
+    ('Shipment', '2026-05-11 16:20:00', '2026-05-11 16:28:15'),    -- 8 min match
+    ('Blood Gulch', '2026-05-11 20:00:00', '2026-05-11 20:45:00'), -- 45 min match
+    ('Facility', '2026-05-12 10:15:00', '2026-05-12 10:30:20'),    -- 15 min match
+    ('Wake Island', '2026-05-12 12:00:00', '2026-05-12 12:42:10'), -- 42 min match
+    ('Dust 2', '2026-05-12 13:30:00', '2026-05-12 13:55:00'),      -- 25 min match
+    ('Nuketown', '2026-05-12 14:10:00', '2026-05-12 14:22:45');    -- 12 min match
