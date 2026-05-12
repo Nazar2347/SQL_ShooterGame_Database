4018f203654b1ccb57a7dfbaa90f0d9d040566bd
KanRuto <dawkapspo@gmail.com>
2026-05-12 16:40:37 +0200

    inventory and loadout

--- Loadout_Table.sql
+++ Loadout_Table.sql
@@ -1,8 +1,21 @@
 CREATE TABLE Loadout(
-	ladout_id INT IDENTITY(1,1) PRIMARY KEY,
-	player_id INT FOREIGN KEY REFERENCES Player(player_id),
-	item_id INT FOREIGN KEY REFERENCES Items(item_id),
-	loadout_name VARCHAR(20) NOT NULL,
-	primary_weapeon_id INT FOREIGN KEY REFERENCES Items(item_id),
-	secondary_weapeon_id INT FOREIGN KEY REFERENCES Items(item_id)
+    loadout_id INT IDENTITY(1,1) PRIMARY KEY,
+    player_id INT FOREIGN KEY REFERENCES Player(player_id),
+    tactical_item_id INT FOREIGN KEY REFERENCES Items(item_id), 
+    loadout_name VARCHAR(20) NOT NULL,
+    primary_weapon_id INT FOREIGN KEY REFERENCES Items(item_id),
+    secondary_weapon_id INT FOREIGN KEY REFERENCES Items(item_id)
 );
+
+INSERT INTO Loadout (player_id, tactical_item_id, loadout_name, primary_weapon_id, secondary_weapon_id)
+VALUES 
+    (1, 5, 'One Shot', 1, 2),
+    (1, 12, 'Night Ops', 1, 4),
+    (2, 7, 'Boom Boom', 2, 3),
+    (3, 8, 'Ghost', 1, 3),
+    (11, 10, 'Healer', 2, 3),
+    (14, 21, 'Plant C4', 2, 7),
+    (29, 14, 'Overpowered', 1, 4),
+    (4, 9, 'Flash Entry', 1, 2),
+    (5, 5, 'Tank', 2, 3),
+    (15, 12, 'Assassin', 1, 4);
--- Transactions.sql
+++ Transactions.sql
@@ -5,3 +5,21 @@ CREATE TABLE Transactions(
 	amount INT DEFAULT 0 NOT NULL,
 	[timestamp] DATETIME2 DEFAULT GETDATE() NOT NULL
 );
+
+INSERT INTO Transactions (player_id, item_id, amount)
+VALUES 
+    (1, 1, 12500),
+    (2, 2, 3500),
+    (11, 10, 3000),
+    (10, 24, 12000),
+    (32, 11, 250),
+    (4, 1, 12500),
+    (40, 2, 3500);
+
+    INSERT INTO Transactions (player_id, item_id, amount, [timestamp])
+VALUES 
+    (15, 12, 8500, '2026-04-09 23:25:00'),
+    (29, 23, 4000, '2026-05-12 14:00:00'),
+    (3, 3, 1200, '2026-01-11 10:00:00'),
+    (5, 5, 2500, '2026-01-21 12:00:00'),   
+    (14, 7, 1800, '2026-04-02 09:15:00');
