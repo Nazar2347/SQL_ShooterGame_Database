01d7c94e584ef626ee947da29f3d5361333cdb6e
Nazarii Dunas <dunas.nazar@gmail.com>
2026-05-13 14:12:14 +0200

    Alterd Friends Table

--- Tables/Friend_Table.sql
+++ Tables/Friend_Table.sql
@@ -3,7 +3,7 @@ CREATE TABLE Friends
 	friendship_id INT IDENTITY(1,1) PRIMARY KEY,
 	inviter_id INT NOT NULL,
 	receiver_id INT NOT NULL,
-	status VARCHAR(20) NOT NULL CHECK (status IN ('pending', 'accepted', 'rejected')),
+	status VARCHAR(20) NOT NULL CHECK (status IN ('pending', 'accepted')),
 	created_at DATETIME DEFAULT GETDATE(),
 	FOREIGN KEY (inviter_id) REFERENCES Player(player_id),
 	FOREIGN KEY (receiver_id) REFERENCES Player(player_id),
